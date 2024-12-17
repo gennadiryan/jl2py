b48e239247e4300d5aae1ee25bdb9ea34476e018
Gennadi Ryan <gennadiryan@gmail.com>
2024-12-16 22:16:32 -0500

    Trying out exporting simple Julia fns to library

--- src/jl2py.jl
+++ src/jl2py.jl
@@ -2,4 +2,16 @@ module jl2py
 
 # Write your package code here.
 
+export _inc32, _inc64, _dec32, _dec64
+
+inc32(x::Int32)::Int32 = x + 1
+inc64(x::Int64)::Int64 = x + 1
+dec32(x::Int32)::Int32 = x - 1
+dec64(x::Int64)::Int64 = x - 1
+
+Base.@ccallable _inc32(x::Cint)::Cint = inc32(x)
+Base.@ccallable _inc64(x::Clonglong)::Clonglong = inc64(x)
+Base.@ccallable _dec32(x::Cint)::Cint = dec32(x)
+Base.@ccallable _dec64(x::Clonglong)::Clonglong = dec64(x)
+
 end
