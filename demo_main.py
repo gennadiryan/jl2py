c7e74218a79161164a5607fe351989f88803be3f
Gennadi Ryan <gennadiryan@gmail.com>
2025-03-02 11:24:07 -0500

    Implemented numpy wrappers for complex matrices; implemented most of UnitarySmoothPulseProblem wrapper

--- demo_main.py
+++ demo_main.py
@@ -8,12 +8,17 @@ from experimental.main import JuliaLib, CDLLUtils, JuliaVal, JuliaValGC, as_obje
 
 """
 TODO:
-    - rebase to integrate Jack's updates
     - add support for additional QCPs/problem templates (e.g. unitary sampling problem)
     - add support for customized initial trajectories
     - add more support for/examples of retrieving data from trajectories
 
     - add QuTIP support
+
+    - get rid of implicit uses of globally defined Julia fns (e.g. getindex)
+
+DONE:
+    - rebase to integrate Jack's updates
+    - add support for the same unitaries/gates as offered by Piccolo.jl
 """
 
 
@@ -40,6 +45,51 @@ def get_tparams(ty):
     _ty_params_len = c_size_t.from_address(_ty_params)
     return [JuliaValGC(c_void_p.from_address(_ty_params + ctypes.sizeof(_ty_params_len) + (ctypes.sizeof(c_void_p) * i))) for i in range(_ty_params_len.value)]
 
+
+def dump_paulis():
+    ks = 'I X Y Z'.split()
+    ret = dict()
+    for k in ks:
+        pauli = getindex(paulis, JuliaValGC(jl.symbol(k.encode())))
+        ret.setdefault(k, complexf64_to_ndarr(pauli))
+    return ret
+
+def dump_gates(gates):
+    ks = 'sqrtiSWAP CX CZ H X XI Y Z I'.split()
+    ret = dict()
+    for k in ks:
+        gate = getindex(gates, JuliaValGC(jl.symbol(k.encode())))
+        ret.setdefault(k, complexf64_to_ndarr(gate))
+    return ret
+
+
+def get_complexf64():
+    return get_global(JuliaValGC(jl.base_module()), 'ComplexF64')
+
+def complexf64_to_ndarr(arr):
+    # prod = lambda f, args: 1 if len(args) == 0 else (args[-1] * f(f, args[:-1]))
+    prod = lambda _: (lambda f: f(f, _))(lambda f, args: 1 if len(args) == 0 else (args[-1] * f(f, args[:-1])))
+
+    shape = tuple(jl.unbox_int64(ptr(getindex(arr.size, JuliaValGC(jl.box_int64(i + 1))))) for i in range(2))
+    size = prod(shape)
+
+    arr = arr_to_ptr(jl, c_double, np.dtype('float64'), (*shape, 2), size * 2, arr)
+    arr = arr.reshape((size, 2)).astype('complex128')
+    arr = (arr[:, 0] + (arr[:, 1] * 1j)).reshape(shape[::-1]).transpose(tuple(range(len(shape)))[::-1])
+    return arr
+
+def ndarr_to_complexf64(ndarr):
+    return JuliaValGC(ptr_to_arr(jl, ptr(get_complexf64()), ndarr.shape[::-1], ndarr.ctypes.data, own=False))
+    
+
+
+# def demo_dump_complexf64_ndarr():
+#     arr = JuliaValGC(jl.eval_string('ComplexF64[0:3;;4:7;;8:11;;;12:15;;16:19;;20:23;;;]'.encode())) # arr.size === (4, 3, 2)
+#     ndarr = arr_to_ptr(jl, c_double, np.dtype('float64'), (48,), 48, arr)
+#     ndarr = ndarr.reshape((24, 2)).astype('complex128')
+#     ndarr = (ndarr[:, 0] + (ndarr[:, 1] * 1j)).reshape((2, 3, 4)).transpose((2, 1, 0)) # arr.size == (4, 3, 2)
+#     return ndarr
+        
 
 
 if __name__ == '__main__':
