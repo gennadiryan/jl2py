241b6974df0b7318683fccbfef10e18d8b261509
Gennadi Ryan <gennadiryan@gmail.com>
2025-03-04 10:04:58 -0500

    It worksgit status

--- demo_main.py
+++ demo_main.py
@@ -321,24 +321,40 @@ class ndarray_from_value(np.ndarray):
 
 
 
-def dump_paulis():
-    # paulis = get_global(mod_qc, 'PAULIS')
-    ks = 'I X Y Z'.split()
-    ret = dict()
-    for k in ks:
-        pauli = getindex(paulis, JuliaValGCv2(jl.symbol(k.encode()))) # implicitly uses `paulis` defined in __main__
-        ret.setdefault(k, complexf64_to_ndarr(pauli))
-    return ret
+# def dump_paulis():
+#     # paulis = get_global(mod_qc, 'PAULIS')
+#     ks = 'I X Y Z'.split()
+#     ret = dict()
+#     for k in ks:
+#         pauli = getindex(paulis, JuliaValGCv2(jl.symbol(k.encode()))) # implicitly uses `paulis` defined in __main__
+#         ret.setdefault(k, complexf64_to_ndarr(pauli))
+#     return ret
 
-def dump_gates():
-    # gates = get_global(mod_qc, 'GATES')
-    ks = 'sqrtiSWAP CX CZ H X XI Y Z I'.split()
-    ret = dict()
-    for k in ks:
-        gate = getindex(gates, JuliaValGCv2(jl.symbol(k.encode()))) # implicitly uses `gates` defined in __main__
-        ret.setdefault(k, complexf64_to_ndarr(gate))
-    return ret
+# def dump_gates():
+#     # gates = get_global(mod_qc, 'GATES')
+#     ks = 'sqrtiSWAP CX CZ H X XI Y Z I'.split()
+#     ret = dict()
+#     for k in ks:
+#         gate = getindex(gates, JuliaValGCv2(jl.symbol(k.encode()))) # implicitly uses `gates` defined in __main__
+#         ret.setdefault(k, complexf64_to_ndarr(gate))
+#     return ret
 
+
+def dump_paulis(copy=True):
+    paulis = get_global(mod_qc, 'PAULIS')
+    ks = 'I X Y Z'.split(' ')
+    arrs = [ndarray_from_value.cast(getindex(paulis, JuliaSymbol(k))) for k in ks]
+    if copy:
+        arrs = [_.copy() for _ in arrs]
+    return dict(zip(ks, arrs))
+
+def dump_gates(copy=True):
+    gates = get_global(mod_qc, 'GATES')
+    ks = 'sqrtiSWAP CX CZ H X XI Y Z I'.split(' ')
+    arrs = [ndarray_from_value.cast(getindex(gates, JuliaSymbol(k))) for k in ks]
+    if copy:
+        arrs = [_.copy() for _ in arrs]
+    return dict(zip(ks, arrs))
 
 
 class QuantumSystem:
@@ -348,19 +364,25 @@ class QuantumSystem:
         h_drives: list[np.ndarray] | None = None,
         **kwargs,
     ) -> None:
-        assert (h_drift is None) or (h_drift.dtype == np.dtype('complex128'))
-        assert (h_drives is None) or (False not in [h_drive.dtype == np.dtype('complex128') for h_drive in h_drives])
-
-        self.h_drift = ndarr_to_complexf64(h_drift) if h_drift is not None else None
-        self.h_drives = ndarrs_to_mat_complexf64(h_drives) if h_drives is not None else None # Julia already handles case of len(h_drives) == 0
-
+        assert (h_drift is None) or (h_drift.dtype == np.dtype('complex128')) # loosen this restriction possibly
+        assert (h_drives is None) or (False not in [h_drive.dtype == np.dtype('complex128') for h_drive in h_drives]) # ditto
+        #
+        # self.h_drift = ndarr_to_complexf64(h_drift) if h_drift is not None else None
+        # self.h_drives = ndarrs_to_mat_complexf64(h_drives) if h_drives is not None else None # Julia already handles case of len(h_drives) == 0
+        self.h_drift = JuliaArr(h_drift) if h_drift is not None else None
+        self.h_drives = [JuliaArr(h_drive) for h_drive in h_drives] if (h_drives is not None) and (len(h_drives) > 0) else None
+        if self.h_drives is not None:
+            h_drives_ty = JuliaType.typeof(self.h_drives[0])
+            self.h_drives = JuliaVec(self.h_drives, h_drives_ty)
+        #
         if len(kwargs) > 0:
             raise NotImplementedError()
-
+        #
         self.args = [_ for _ in (self.h_drift, self.h_drives) if _ is not None]
         self.kwargs = dict([_ for _ in kwargs.items()]) # noop for the time being
-
-        self.value = fn_qs(*self.args)
+        #
+        # self.value = fn_qs(*self.args)
+        self.value = get_global(mod_qc, 'QuantumSystem')(*self.args)
 
 
 
@@ -368,8 +390,9 @@ class QuantumControlProblem:
     def solve(self, **kwargs) -> None:
         if len(kwargs) > 0:
             raise NotImplementedError()
-        
-        fn_solve(self.value)
+        #
+        # fn_solve(self.value)
+        get_global(mod_qc, 'solve!')(self.value)
 
 
 class QuantumStateSmoothPulseProblem(QuantumControlProblem):
@@ -384,20 +407,26 @@ class QuantumStateSmoothPulseProblem(QuantumControlProblem):
     ) -> None:
         super().__init__() # maybe handle kwargs (Piccolo/Ipopt) options here?
 
+        assert len(states_init) > 0
+        assert len(states_goal) > 0
+
         assert False not in [state_init.dtype == np.dtype('complex128') for state_init in states_init]
         assert False not in [state_goal.dtype == np.dtype('complex128') for state_goal in states_goal]
 
-        self.system = system.value
-        self.states_init = ndarrs_to_mat_complexf64(states_init)
-        self.states_goal = ndarrs_to_mat_complexf64(states_goal)
+        self.system = system
+        # self.states_init = ndarrs_to_mat_complexf64(states_init)
+        # self.states_goal = ndarrs_to_mat_complexf64(states_goal)
+        self.states_init, self.states_goal = [JuliaVec(states, JuliaType.typeof(states[0])) for states in [[JuliaArr(state) for state in states] for states in (states_init, states_goal)]]
 
-        self.T = JuliaValGC(jl.box_int64(T))
-        self.dt = JuliaValGC(jl.box_float64(dt)) if not isinstance(dt, np.ndarray) else JuliaValGC(ptr_to_arr(jl, jl.float64_type(), dt.shape, dt.ctypes.data, own=False))
+        # self.T = JuliaValGC(jl.box_int64(T))
+        self.T = JuliaInt(T)
+        # self.dt = JuliaValGC(jl.box_float64(dt)) if not isinstance(dt, np.ndarray) else JuliaValGC(ptr_to_arr(jl, jl.float64_type(), dt.shape, dt.ctypes.data, own=False))
+        self.dt = JuliaFloat(dt)
 
         if len(kwargs) > 0:
             raise NotImplementedError()
         
-        self.value = fn_qsspp(self.system, self.states_init, self.states_goal, self.T, self.dt)
+        self.value = get_global(mod_qc, 'QuantumStateSmoothPulseProblem')(self.system.value, self.states_init, self.states_goal, self.T, self.dt)
 
 
 class UnitarySmoothPulseProblem(QuantumControlProblem):
@@ -406,23 +435,29 @@ class UnitarySmoothPulseProblem(QuantumControlProblem):
         system: QuantumSystem,
         operator: np.ndarray,
         T: int,
-        dt: float | np.ndarray,
+        # dt: float | np.ndarray, # TODO
+        dt: float,
         **kwargs,
     ) -> None:
         super().__init__() # maybe handle kwargs (Piccolo/Ipopt) options here?
 
         assert operator.dtype == np.dtype('complex128')
-        assert (not isinstance(dt, np.ndarray)) or (dt.dtype == np.dtype('float64'))
+        # assert (not isinstance(dt, np.ndarray)) or (dt.dtype == np.dtype('float64')) # TODO
 
-        self.system = system.value
-        self.operator = ndarr_to_complexf64(operator)
-        self.T = JuliaValGC(jl.box_int64(T))
-        self.dt = JuliaValGC(jl.box_float64(dt)) if not isinstance(dt, np.ndarray) else JuliaValGC(ptr_to_arr(jl, jl.float64_type(), dt.shape, dt.ctypes.data, own=False))
+        # self.system = system.value
+        # self.operator = ndarr_to_complexf64(operator)
+        # self.T = JuliaValGC(jl.box_int64(T))
+        # self.dt = JuliaValGC(jl.box_float64(dt)) if not isinstance(dt, np.ndarray) else JuliaValGC(ptr_to_arr(jl, jl.float64_type(), dt.shape, dt.ctypes.data, own=False))
+        
+        self.system = system
+        self.operator = JuliaArr(operator)
+        self.T = JuliaInt(T)
+        self.dt = JuliaFloat(dt)
 
         if len(kwargs) > 0:
             raise NotImplementedError()
         
-        self.value = fn_uspp(self.system, self.operator, self.T, self.dt)
+        self.value = get_global(mod_qc, 'UnitarySmoothPulseProblem')(self.system.value, self.operator, JuliaInt(T), JuliaFloat(dt))
 
 
 
@@ -669,4 +704,49 @@ if __name__ == '__main__':
     # # data_mat = data_vec.reshape((dim_rows, dim_cols)).transpose()
     
     # # rng_a, = [getindex(prob.value.trajectory.components, JuliaValGC(jl.symbol(_.encode()))) for _ in ('a',)]
+    
+
+
+    # Remember to ask about "libc++abi: terminating due to uncaught exception of type Ipopt::RESTORATION_MAXITER_EXCEEDED"
+
+
+    paulis = dump_paulis(copy=True)
+    gates = dump_gates(copy=True)
+
+
+    def demo_unitary_smooth_pulse_problem():
+        system = QuantumSystem(h_drives=[paulis['X'], paulis['Y']])
+        problem = UnitarySmoothPulseProblem(system, gates['H'], 50, 0.2)
+        problem.value.ipopt_options.max_iter = JuliaInt(50) # hack until we finish implementing kwargs and/or IpoptOptions/PiccoloOptions
+
+        fidelity_initial = JuliaFloat.cast(get_global(mod_qc, 'unitary_fidelity')(problem.value))
+        problem.solve()
+        fidelity_final = JuliaFloat.cast(get_global(mod_qc, 'unitary_fidelity')(problem.value))
+        assert fidelity_final > fidelity_initial
+        print(f'unitary_fidelity=(before={fidelity_initial},after={fidelity_final})')
+        print()
+
+        plot = get_global(mod_qc, 'plot_unitary_populations')(problem.value.trajectory)
+        display = get_global(mod_qc, 'display')(plot)
+
+
+    def demo_quantum_state_smooth_pulse_problem():
+        system = QuantumSystem(h_drift=(0.1 * gates['Z']), h_drives=[gates['X'], gates['Y']])
+        state_init = np.array([1., 0.], dtype=np.dtype('complex128'))
+        state_goal = np.array([0., 1.], dtype=np.dtype('complex128'))
+        problem = QuantumStateSmoothPulseProblem(system, [state_init], [state_goal], 50, 0.2)
+        problem.value.ipopt_options.max_iter = JuliaInt(50) # hack until we finish implementing kwargs and/or IpoptOptions/PiccoloOptions
+        
+        fidelity_initial = JuliaFloat.cast(get_global(mod_qc, 'fidelity')(problem.value.trajectory, system.value))
+        problem.solve()
+        fidelity_final = JuliaFloat.cast(get_global(mod_qc, 'fidelity')(problem.value.trajectory, system.value))
+        assert fidelity_final > fidelity_initial
+        print(f'fidelity=(before={fidelity_initial},after={fidelity_final})')
+        print()
+
+
+    demo_unitary_smooth_pulse_problem()
+    demo_quantum_state_smooth_pulse_problem()
+    print('Done!')
+
     
