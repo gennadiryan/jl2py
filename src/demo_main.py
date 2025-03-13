d2d64f44c842aaea2a841bd7c97607bc3d4d125d
Gennadi Ryan <gennadiryan@gmail.com>
2025-03-13 16:54:15 -0400

    Renamed top-level package from jl2py to pypiccolo; sub-package julia to be spun off as jl2py

--- src/demo_main.py
+++ src/demo_main.py
@@ -6,12 +6,12 @@ from ctypes import cdll, c_double, c_float, c_int, c_int32, c_int64, c_uint, c_u
 import numpy as np
 
 # from experimental.main import JuliaLib, CDLLUtils, JuliaVal, JuliaValGC, as_object, ptr_to_arr, arr_to_ptr, get_ctypes_arr, init_JuliaVal, init_JuliaValGC, add_ref, del_ref
-from jl2py.julia.julia_value import init_jl, ptr_to_arr, arr_to_ptr, get_ctypes_arr, get_nt, JuliaVal, JuliaValGC
-from jl2py.julia.julia_extras import ptr, get_global, call_with_kwargs, JuliaType, JuliaNum, JuliaInt, JuliaFloat, JuliaComplex, JuliaSymbol, JuliaVec, JuliaArr, ndarray_from_value, println, getindex
+from pypiccolo.julia.julia_value import init_jl, ptr_to_arr, arr_to_ptr, get_ctypes_arr, get_nt, JuliaVal, JuliaValGC
+from pypiccolo.julia.julia_extras import ptr, get_global, call_with_kwargs, JuliaType, JuliaNum, JuliaInt, JuliaFloat, JuliaComplex, JuliaSymbol, JuliaVec, JuliaArr, ndarray_from_value, println, getindex
 
-from jl2py.quantumcollocationpy import *
-from jl2py.quantumcollocationpy.quantumcollocation import QuantumSystem
-from jl2py.quantumcollocationpy.problemtemplates import QuantumStateSmoothPulseProblem, UnitarySmoothPulseProblem, UnitaryMinimumTimeProblem
+from pypiccolo.quantumcollocationpy import *
+from pypiccolo.quantumcollocationpy.quantumcollocation import QuantumSystem
+from pypiccolo.quantumcollocationpy.problemtemplates import QuantumStateSmoothPulseProblem, UnitarySmoothPulseProblem, UnitaryMinimumTimeProblem
 
 
 """
--- tests/test.py
+++ tests/test.py
@@ -4,14 +4,11 @@
 
 import numpy as np
 
-from jl2py.quantumcollocationpy import PAULIS
-from jl2py.quantumcollocationpy import GATES
-from jl2py.quantumcollocationpy.quantumcollocation import QuantumSystem
-from jl2py.quantumcollocationpy.problemtemplates import UnitarySmoothPulseProblem
-from jl2py.quantumcollocationpy.problemtemplates import UnitaryMinimumTimeProblem
-from jl2py.quantumcollocationpy.quantumcollocation import unitary_fidelity
-from jl2py.quantumcollocationpy.quantumcollocation import plot_unitary_populations
-from jl2py.quantumcollocationpy.quantumcollocation import traj_to_mat
+from pypiccolo.quantumcollocationpy import PAULIS
+from pypiccolo.quantumcollocationpy import GATES
+from pypiccolo.quantumcollocationpy.problemtemplates import QuantumStateSmoothPulseProblem, UnitarySmoothPulseProblem, UnitaryMinimumTimeProblem
+from pypiccolo.quantumcollocationpy.quantumcollocation import QuantumSystem
+from pypiccolo.quantumcollocationpy.quantumcollocation import unitary_fidelity, plot_unitary_populations, traj_to_mat
 
 system = QuantumSystem(h_drives=[PAULIS['X'], PAULIS['Y']])
 problem = UnitarySmoothPulseProblem(system, GATES['H'], 50, 0.2)
