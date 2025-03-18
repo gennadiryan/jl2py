e93abe4f3821b0a970222b711be803c11baf2ad9
Gennadi Ryan <gennadiryan@gmail.com>
2025-03-18 11:28:51 -0400

    Documented important paths

--- demo/lib_relocatable.py
+++ demo/lib_relocatable.py
@@ -1,10 +1,21 @@
+import sys
+import os
+
 import ctypes
 from ctypes import *
-lib = ctypes.cdll.LoadLibrary('/Users/gennadiryan/Documents/kestrel/jl2py/target/lib/libjulia.dylib')
+
+rootdir = '/Users/gennadiryan/Documents/kestrel/jl2py/target'
+bindir = os.path.join(rootdir, 'bin') # placeholder; directory need not exist (only passed since Julia uses JULIA_BINDIR to set default system search paths; may be unnecessary)
+libdir = os.path.join(rootdir, 'lib')
+depotdir = os.path.join(rootdir, 'share', 'julia') # must be sysimage DEPOT_PATH/LOAD_PATH
+
+libjulia = os.path.join(libdir, 'libjulia.dylib') # can be generic libjulia
+libpiccolo = os.path.join(libdir, 'libpiccolo.dylib') # must be sysimage
+
+lib = ctypes.cdll.LoadLibrary(libjulia)
 lib.jl_init_with_image.argtypes = [c_char_p, c_char_p]
 lib.jl_init_with_image.restype = None
 lib.jl_eval_string.argtypes = [c_char_p]
 lib.jl_eval_string.restype = c_void_p
 
-lib.jl_init_with_image(b'/Users/gennadiryan/Documents/kestrel/jl2py/target/bin', b'/Users/gennadiryan/Documents/kestrel/jl2py/target/lib/libpiccolo.dylib')
-
+lib.jl_init_with_image(bindir.encode(), libpiccolo.encode())
